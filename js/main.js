9b44a0d1b836baf8fc7aaa89ecd02a6062b8e3c9
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2020-10-31 11:27:35 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+//day 1
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+
+buttonAuth.addEventListener('click', toggleModalAuth);
+closeAuth.addEventListener('click', toggleModalAuth);
